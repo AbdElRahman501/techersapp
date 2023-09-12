c99d244e748b98867aac8878aa97247e375d7c15
AbdElRahman501 <AbdElRahman501>
2023-09-12 22:34:52 +0300

    search screen ,message,splash screen

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,52 +1,17 @@
-import { StyleSheet, Animated, TextInput, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { StyleSheet, View, TouchableOpacity, Text } from 'react-native'
+import React from 'react'
 import { Search_icon_Svg } from '../assets/icons/Icons';
-import { Border, Color, FontFamily, FontSize, Padding, heightPercentage, widthPercentage } from '../GlobalStyles';
+import { Border, Color, FontFamily, FontSize, widthPercentage } from '../GlobalStyles';
 import { useSelector } from 'react-redux';
-import { getTextInputAlign } from '../actions/GlobalFunctions';
 import t from '../actions/changeLanguage';
-import transition from '../actions/transition';
-import SearchResults from './SearchResults';
-import BackHeader from './BackHeader';
+import SearchModal from './SearchModal';
 
 export default function SearchBar({ isFocused, setIsFocused }) {
   const { language } = useSelector(state => state.languageState);
-  const [value, setValue] = useState("")
-  const [results, setResults] = useState(false);
-  const [yPosition, setYPosition] = useState(0)
-
-
-  let Top = transition(yPosition, 0, 350, isFocused)
-  let Bot = transition(-yPosition, 0, 350, !isFocused)
-  let Height = transition(64, heightPercentage(100)+20, 350, isFocused)
-
-  useEffect(() => {
-    if (isFocused) {
-      setTimeout(() => {
-        setResults(true)
-      }, 150)
-    } else {
-      setResults(false)
-    }
-  }, [isFocused])
 
   return (
-    <Animated.View style={[styles.container, {
-      height: Height,
-      top: isFocused ? Top : Bot,
-      position: isFocused ? "absolute" : "relative",
-    }]}
-      onLayout={(event) => {
-        const { x, y, width, height } = event.nativeEvent.layout;
-        if (!yPosition) {
-          setYPosition(y)
-        }
-      }}
-    >
-      {results &&
-        <BackHeader title={"search"} onPress={true} onPressHandler={() => setIsFocused(false)} />
-      }
-      <View style={[styles.inputField, { flexDirection: language === "ar" ? "row-reverse" : "row" }]}>
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => setIsFocused(!isFocused)} style={[styles.inputField, { flexDirection: language === "ar" ? "row-reverse" : "row" }]}>
         <View style={styles.rightIcon}>
           <Search_icon_Svg
             width={30}
@@ -55,22 +20,12 @@ export default function SearchBar({ isFocused, setIsFocused }) {
             color={isFocused ? Color.darkcyan : Color.input_stroke}
           />
         </View>
-        <TextInput style={[styles.input, {
-          textAlign: language === "en" ? "left" : (getTextInputAlign(value) || "right"),
-          paddingLeft: (language === "ar" && getTextInputAlign(value) === "left") ? 46 : 0
-        }]}
-          placeholder={t("search")}
-          autoCapitalize="none"
-          onChangeText={e => setValue(e)}
-          value={value}
-          keyboardType={"default"}
-          onFocus={() => setIsFocused(true)}
-        // onBlur={() => setIsFocused(false)}
-        />
-
-      </View>
-      {results && <SearchResults value={value} />}
-    </Animated.View>
+        <Text style={[styles.input]}>
+          {t("search")}
+        </Text>
+      </TouchableOpacity>
+      <SearchModal isFocused={isFocused} setIsFocused={setIsFocused} />
+    </View>
   )
 }
 
@@ -78,7 +33,6 @@ const styles = StyleSheet.create({
   container: {
     width: widthPercentage(100),
     alignItems: "center",
-    zIndex: 999999,
     backgroundColor: Color.white,
   },
   inputField: {
@@ -93,7 +47,12 @@ const styles = StyleSheet.create({
     flex: 1,
     height: 46,
     fontFamily: FontFamily.montserratArabic,
-    fontSize: FontSize.size_base
+    fontSize: FontSize.size_base,
+    textAlignVertical: "center",
+    fontSize: FontSize.size_base,
+    fontFamily: FontFamily.montserratArabic,
+    color: Color.darkgray
+
   },
   rightIcon: {
     paddingTop: 8,
